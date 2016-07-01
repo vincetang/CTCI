1f8f500dc1e40f28d5004df30d9b1fcde6858911
Vincent Tang <Vince@MacBook-Pro.local>
2016-06-30 23:57:09 -0400

    reorganized files

--- CTCI_2_Linked_Lists/src/CTCI_2/CTCI_2.java
+++ CTCI_2_Linked_Lists/src/CTCI_2/CTCI_2.java
@@ -1,8 +1,13 @@
 package CTCI_2;
 
+import java.util.Map;
+
 public class CTCI_2 {
 
-	// 2.1
+	/** 2.1 
+	 *	Write code to remove duplicates from an unsorted linked list.
+	 *	FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
+	 */
 	public void removeDuplicates(Node head) {
 		Node curNode = head;
 		
@@ -18,14 +23,102 @@ public class CTCI_2 {
 		}
 	}
 
+	/** 2.2
+	 *	Implement an algorithm to find the kth to last element of a singly linked list. 
+	 */
+	public int getLength(Node head) {
+		int length = 1;
+		Node curNode = head;
+		while (curNode.next != null) {
+			length++;
+			curNode = curNode.next;
+		}
+		return length;
+	}
+	
+	public Node getKthToLastElement(int k, Node head) {
+		int length = getLength(head);
+		
+		if (k > (length - 1)) {
+			return null;
+		}
+		
+		Node curNode = head;
+		int position = 0;
+		while (position < (length - (k+1))) {
+			curNode = curNode.next;
+			position++;
+		}
+		return curNode;
+	}
+	
+	/* recursive solution */
+	public Node getKthToLastRecursive(int k, Node head) {
+		if (k > this.getLength(head) - 1) {
+			return null;
+		}
+		MapEntry<Node, Integer> ent = getKthHelper(k, head);
+		return (Node) ent.getKey();
+	}
+	
+	public MapEntry<Node, Integer> getKthHelper(int k, Node head) {
+		Node curNode = head;
+		
+		if (curNode.next == null) {
+			return new MapEntry<Node, Integer>(curNode, 0);
+		} else {
+			MapEntry<Node, Integer> ent = getKthHelper(k, curNode.next);
+			
+			if (ent.getValue().intValue() == k)
+				return ent;
+			else
+				return new MapEntry<Node, Integer>(curNode, ent.getValue()+1);
+		}
+	}
+	
+	/** 2.3
+	 * Implement an algorithm to delete a node in the middle of a singly linked list,
+	 * given only access to that node
+	 * EXAMPLE
+	 * Input: the node c from the linked list a > b > c > d > e
+	 * Result: Nothing is returned, but the new linked list looks like a > b > d > e
+	 */
+	
+	/** 2.4
+	 * Write code to partition a linked list around a value x, such that all nodes less
+	 * than x come before all nodes greater than or equal to x
+	 */
+	
+	/** 2.5
+	 * You have two numbers represented by a linked list, where each node contains a
+	 * single digit. The digits are stored in reverse order, such that the 1's digit
+	 * is at the head of the list. Write a function that adds the two numbers and 
+	 * returns the sum as a linked list
+	 * EXAMPLE
+	 * Input: (7>1>6) + (5>9>2). That is, 617 + 295
+	 * Output: 2>1>9. That is, 912
+	 * FOLLOW UP
+	 * Suppose the digits are stored in forward order. Repeat the above problem
+	 * EXAMPLE
+	 * Input: (6>1>7) + (2>9>5). That is, 617 + 295.
+	 * Output: 9>1>2. That is, 912.
+	 */
+	
+	/** 2.6
+	 * Given a circular linked list, implement an algorithm which returns the node
+	 * of the beginning of the loop
+	 * DEFINITION
+	 * Circular linked list: A (corrupt) linked list in which a node's next pointer 
+	 * points to an earlier node, so as to make a loop in the linked list
+	 * EXAMPLE
+	 * Input: A>B>C>D>E>C[the same C as earlier]
+	 * Output: C
+	 */
+	
+	/** 2.7
+	 * Implement a function to check if a linked list is a palindrome.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Node node1 = new Node(1);
-		Node node2 = new Node(2);
-		Node node3 = new Node(3);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-		node1.append(node2);
-		node1.append(node3);
 
 	}
 
--- CTCI_2_Linked_Lists/src/CTCI_2/TestJunit.java
+++ CTCI_2_Linked_Lists/src/CTCI_2/TestJunit.java
@@ -5,26 +5,157 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.ArrayComparisonFailure;
 
 public class TestJunit {
 
+	private Node node1, node2, node3, node4, node5, node6, node7, node8;
+	private CTCI_2 ctci;
 	@Before
 	public void setUp() throws Exception {
-		Node node1 = new Node(1);
-		Node node2 = new Node(2);
-		Node node3 = new Node(4);
-		Node node4 = new Node(5);
-		Node node6 = new Node(7);
-		Node node8 = new Node(8);
+		ctci = new CTCI_2();
+		node1 = new Node(1);
+		node2 = new Node(2);
+		node3 = new Node(3);
+		node4 = new Node(3);
+		node5 = new Node(5);
+		node6 = new Node(6);
+		node7 = new Node(7);
+		node8 = new Node(8);
+		
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		node5.next = node6;
+		node6.next = node7;
+		node7.next = node8;
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	/** Testing Node Structures **/
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void singleNodeTest() {
+		assertEquals(1, node1.data);
 	}
+	
+	@Test
+	public void nullNext() {
+		assertNull(node8.next);
+	}
+	
+	@Test
+	public void nullNotNext() {
+		assertNotNull(node1.next);
+	}
+	
+	@Test 
+	public void nextNode() {
+		assertEquals(node2, node1.next);
+	}
+	
+	/** 2.1 removeDuplicate Tests **/
+	@Test
+	public void duplicateRemoved() {
+		ctci.removeDuplicates(node1);
+		assertEquals(node5, node3.next);
+	}
+	
+	@Test
+	public void noDuplicates() {
+		ctci.removeDuplicates(node6);
+		assertEquals(6, node6.data);
+		assertEquals(7, node6.next.data);
+		assertEquals(8, node6.next.next.data);
+		assertNull(node6.next.next.next);
+	}
+	
+	/** 2.2 **/
+	/* Length Helper Function */
+	@Test
+	public void fullLength() {
+		assertEquals(8, ctci.getLength(node1));
+	}
+	
+	@Test
+	public void singleLength() {
+		assertEquals(1, ctci.getLength(node8));
+	}
+	
+	@Test
+	public void middleLength() {
+		assertEquals(3, ctci.getLength(node6));
+	}
+	
+	/* getKthToLastElement() */
 
+	@Test
+	public void lastElement() {
+		Node ret = ctci.getKthToLastElement(0, node1);
+		assertEquals(node8, ret);
+	}
+	
+	@Test
+	public void firstElement() {
+		Node ret = ctci.getKthToLastElement(7, node1);
+		assertEquals(node1, ret);
+	}
+	
+	@Test
+	public void middleElement() {
+		Node ret = ctci.getKthToLastElement(3, node1);
+		assertEquals(node5, ret);
+	}
+	
+	@Test
+	public void nullElement() {
+		assertNull(ctci.getKthToLastElement(9, node1));
+	}
+	
+	/* getKthToLastRecursive() */
+
+	@Test
+	public void lastElementRecursive() {
+		Node ret = ctci.getKthToLastRecursive(0, node1);
+		try {
+			assertEquals(node8, ret);
+		} catch (AssertionError e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void firstElementRecursive() {
+		Node ret = ctci.getKthToLastRecursive(7, node1);
+		try {
+			assertEquals(node1, ret);
+		} catch (AssertionError e) {
+			System.out.println("Failed middleElementRecursive(). Expected:" + node5.data + " got:" + ret.data);
+		}
+	}
+	
+	@Test
+	public void middleElementRecursive() {
+		Node ret = ctci.getKthToLastRecursive(3, node1);
+
+		try {
+			assertEquals(node5, ret);
+		} catch (AssertionError e) {
+			
+		}
+
+		
+	}
+	
+	@Test
+	public void nullElementRecursive() {
+		try {
+			assertNull(ctci.getKthToLastRecursive(9, node1));
+		} catch (AssertionError e) {
+			e.printStackTrace();
+		}
+	}
 }
